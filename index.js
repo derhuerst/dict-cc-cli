a345235f573d7ac6a551f679647f41edaacb7476
Jannis R <mail@jannisr.de>
2017-01-02 18:31:18 +0100

    WIP implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 'use strict'
 
-// todo
+const searchIndex = require('search-index')
+const normalizer = require('docproc').pipeline
+const path = require('path')
+const sink = require('stream-sink')
+
+
+
+const tokenize = (query) => new Promise((yay, nay) => {
+	const pipeline = normalizer()
+	pipeline.once('data', (data) => yay(data.tokenised))
+	pipeline.once('error', nay)
+	pipeline.end(query)
+})
+
+const search = (index, tokenized) =>
+	index.search({
+		query: [{AND: tokenized}],
+		pageSize: 10
+	})
+	.pipe(sink('object'))
+
+
+
+const dict = () => {
+	let index
+	const init = () => new Promise((yay, nay) => {
+		if (index) return yay(index)
+		searchIndex({
+			levelPath: path.join(__dirname, 'index.leveldb')
+		}, (err, _index) => {
+			if (err) return nay(err)
+			index = _index
+			yay(index)
+		})
+	})
+
+	const search = (query) => {
+		return init().then((index) =>
+			tokenize(query)
+			.then((tokenized) => {
+				console.log(tokenized)
+				return search(index, tokenized)
+			})
+		)
+	}
+	return search
+}
+
+module.exports = dict
+
+// module.exports = {tokenize, search}
