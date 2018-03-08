553535cb4123d8101019a730b3d9361d57cb47f6
Jannis R <mail@jannisr.de>
2018-03-08 01:52:27 +0100

    better error handling

--- bin.js
+++ bin.js
@@ -24,7 +24,8 @@ if (argv.version || argv.v) {
 }
 
 const showError = (err) => {
-	console.error(err)
+	if (process.env.NODE_DEBUG === 'dict-cc-cli') console.error(err)
+	else console.error(err.message || (err + ''))
 	process.exit(1)
 }
 
@@ -38,4 +39,8 @@ dict(query)
 .on('data', ([de, en, type]) => {
 	console.log(chalk.yellow(de), chalk.blue(en), type ? chalk.gray(type) : '')
 })
-.on('error', showError)
+.on('error', (err) => {
+	if (err.code === 'ENOENT') {
+		showError('Couldn\'t find the data file. Run dict-cc-import first.')
+	} else showError(err)
+})
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const normalize = require('normalize-for-search')
 const fs = require('fs')
 const path = require('path')
 const ndjson = require('ndjson')
-const filter = require('stream-filter')
+const filterStream = require('stream-filter')
 const words = require('word-regex')()
 
 
@@ -12,9 +12,9 @@ const words = require('word-regex')()
 const dict = (query) => {
 	const tokens = normalize(query).match(words)
 
-	return fs.createReadStream(path.join(__dirname, 'data.ndjson'))
-	.pipe(ndjson.parse())
-	.pipe(filter.obj((row) => { // OR search
+	const reader = fs.createReadStream(path.join(__dirname, 'data.ndjson'))
+	const parser = ndjson.parse()
+	const filter = filterStream.obj((row) => { // OR search
 		const [de, en, type] = row
 
 		const enTokens = normalize(en).match(words)
@@ -24,7 +24,11 @@ const dict = (query) => {
 		if (tokens.every((t) => deTokens.includes(t))) return true
 
 		return false
-	}))
+	})
+
+	reader.once('error', err => parser.destroy(err))
+	parser.once('error', err => filter.destroy(err))
+	return reader.pipe(parser).pipe(filter)
 }
 
 module.exports = dict
